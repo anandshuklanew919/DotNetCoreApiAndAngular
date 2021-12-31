26f27f523eb7dcc7ee60205ead0375829e243e16
Anand Shukla <anandshuklanew919@gmail.com>
2021-12-31 19:33:07 +0530

    End of section 16

--- API/DTO/AddressDto.cs
+++ API/DTO/AddressDto.cs
@@ -1,17 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.DTO
 {
     public class AddressDto
-    {
+    {   [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Street { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
         public string ZipCode { get; set; }
     }
 }
--- API/Extensions/SwaggerServiceExtension.cs
+++ API/Extensions/SwaggerServiceExtension.cs
@@ -13,6 +13,22 @@ namespace API.Extensions
              services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorizatioin",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement{{securitySchema, new []{"Bearer"}}};
+                c.AddSecurityRequirement(securityRequirement);
             });
 
             return services;
